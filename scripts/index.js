782e56161552ab88cb6c16a1e3ba8f025a8ec0ac
vlada967 <vladaarhipova3@gmail.com>
2022-04-27 13:10:23 +0700

    fixed mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,44 @@
 let popupBackground = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
 let popupName = document.querySelector('.popup__text_type_name');
 let popupJob = document.querySelector('.popup__text_type_job');
-
 let popupElement = document.querySelector('.popup__form');
 
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__save-button');
 
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
 
-let likeElement = document.querySelectorAll('.element__like');
-
-popupName.value = name.textContent;
-popupJob.value = job.textContent;
+//let likeElement = document.querySelectorAll('.element__like');
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
     name.textContent = popupName.value;
     job.textContent = popupJob.value;
+
+    closePopup();
+}
+
+function openPopup() {
+    popupName.value = name.textContent;
+    popupJob.value = job.textContent;
+
+    popupBackground.classList.toggle('popup_opened');
+}
+
+function closePopup() {
+    popupBackground.classList.toggle('popup_opened');
 }
 
 popupElement.addEventListener('submit', formSubmitHandler);
 
-editButton.addEventListener('click', popup);
-closeButton.addEventListener('click', popup);
-saveButton.addEventListener('click', popup);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
-function popup() {
-    formElement.classList.toggle('popup_opened');
-    popupBackground.classList.toggle('popup_opened');
-}
-
-for (let i = 0 ; i < likeElement.length; i++) {
+/*for (let i = 0; i < likeElement.length; i++) {
     likeElement[i].addEventListener('click', function() {
         likeElement[i].classList.toggle("element__like_active")
     });
     ;
-}
+}*/
